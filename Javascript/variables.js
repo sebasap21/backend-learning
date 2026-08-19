b882d1f3221db64e0e255eaf09f9bbf2edaf1136
sebas21Ap <aarainga@ucvvirtual.edu.pe>
2026-08-19 12:16:36 -0500

    feat: add variables and functions exercise

---
+++ Javascript/variables.js
@@ -0,0 +1,14 @@
+const nombre = "Andres";
+const edad = "22";
+const carrera = "Ing.sistemas";
+const ciclo = "8";
+
+function showInfo(){
+    console.log("Nombre: "+nombre);
+    console.log("Edad: "+edad);
+    console.log("Carrera: "+carrera);
+    console.log("Ciclo: "+ciclo);
+
+}
+
+showInfo()
